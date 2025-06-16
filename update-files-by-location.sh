ea21b92373cb30f0fbf79b6360238b892b2c44ad
Keith <kayceecode@gmail.com>
2025-06-15 23:59:47 -0400

    update files

--- update-files-by-location.sh
+++ update-files-by-location.sh
@@ -1,3 +1,6 @@
  cp ~/.tmux/tmux-powerline/config.sh ~/dotfiles/tmux/tmux-powerline/
+ cp ~/.tmux/tmux-powerline/config/default.sh ~/dotfiles/tmux/tmux-powerline/config/
  cp ~/.tmux/tmux-powerline/themes/default.sh ~/dotfiles/tmux/tmux-powerline/themes/
  cp /org/cinnamon/desktop/keybindings/ ~/dotfiles/org/cinnamon/desktop/keybindings/
+ cp ~/workspace-move-docker-desktop.sh ~/dotfiles/
+
